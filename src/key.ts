36acc8463447ce4670fcc5dca230ec5190301589
Ab Reitsma <abreits@gmail.com>
2018-04-22 09:43:03 +0200

    time to double

--- src/key.ts
+++ src/key.ts
@@ -6,8 +6,8 @@ import * as fs from "fs";
 
 import { KEY_LENGTH } from "./crypto-message";
 
-const MAX_DATE = new Date(738905609893065); //  2^47 (because we translate it to a six byte int)
-const MIN_DATE = new Date(-738905609893065);
+const MAX_DATE = new Date(8640000000000000);
+const MIN_DATE = new Date(-8640000000000000);
 /**
  *  key class, defines a new key and initializes it with a random key and id
  */
@@ -64,16 +64,15 @@ export class Key {
             if (!this.id || !this.key) {
                 throw new Error("Trying to encrypt incomplete Key");
             }
-            const activateOff = Buffer.allocUnsafe(6);
-            activateOff.writeIntLE((this.activateOff ? this.activateOff : MAX_DATE).getTime(), 0, 6);
+            const activateOff = Buffer.allocUnsafe(8);
+            activateOff.writeDoubleLE((this.activateOff ? this.activateOff : MAX_DATE).getTime(), 0);
             this.toEncrypt = Buffer.concat([activateOff, this.key, this.id]);
         }
         if (!this.signed) {
-            const sign = crypto.createSign("SHA1");
+            const sign = crypto.createSign("SHA256");
             sign.update(this.toEncrypt);
             this.signed = sign.sign(privateKey);
         }
-        // encrypted size is 256 bytes
         const encrypted = crypto.publicEncrypt(publicKey, this.toEncrypt);
         return (Buffer.concat([encrypted, this.signed]));
     }
@@ -88,13 +87,13 @@ export class Key {
 
     static decrypt(encrypted: Buffer, privateKey: string, publicKey: string) {
         const decrypted = crypto.privateDecrypt(privateKey, encrypted.slice(0, 256));
-        const verify = crypto.createVerify("SHA1");
+        const verify = crypto.createVerify("SHA256");
         verify.update(decrypted);
         if(verify.verify(publicKey, encrypted.slice(256))) {
             const key = new Key();
-            key.activateOff = new Date(decrypted.readIntLE(0,6));
-            key.key = decrypted.slice(6, 38);
-            key.id = decrypted.slice(38);
+            key.activateOff = new Date(decrypted.readDoubleLE(0));
+            key.key = decrypted.slice(8, 40);
+            key.id = decrypted.slice(40);
             return key;
         } else {
             throw new Error("Key signature failed!");
