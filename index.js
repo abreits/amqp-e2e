4e048c6592a005b06b25e78d320fd6d37426c69c
Ab Reitsma <abreits@gmail.com>
2018-06-12 12:59:43 +0200

    update for docker production

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  *
  * starts different functionality for this image, based on environment variable settings
  * currently supported settings:
- * START: 'encrypt', 'decrypt' or 'dev'
- *  - encrypt: starts an encryption shovel
- *  - decrypt: starts a decryption shovel
+ * START: 'run' or 'dev'
+ *  - run: starts a shovel (default)
  *  - dev: starts a docker development and test environment for the application
  */
 
@@ -15,21 +14,13 @@ if (start) {
 }
 
 switch (start) {
-    case "encrypt":
-        console.log("ERROR: 'encrypt' start not implemented yet.");
-        console.log("Exiting!");
-        break;
-    case "decrypt":
-        console.log("ERROR: 'decrypt' start not implemented yet.");
-        console.log("Exiting!");
-        break;
     case "dev":
         console.log("Starting development environment.");
         require("./tools/alive");
         break;
     default:
-        console.log("ERROR: expected a START environment variable with value of 'encrypt', 'decrypt' or 'dev'.");
-        console.log("Exiting!");
+        console.log("Starting amqp-e2e shovel.");
+        require("run/index"); // only works in docker-production version
 }
 
 
